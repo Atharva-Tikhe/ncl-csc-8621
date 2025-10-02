93ee1a79f054dcda6b39a2022e7b3710b82e26ab
atharva.tikhe <atharva.tikhe@medgenome.com>
2025-10-02 14:01:00 +0100

    test runner (replace with pytest/unittest)

--- week2/practical6/main.py
+++ week2/practical6/main.py
@@ -1,8 +1,9 @@
 from car import Car
+from car_manager import CarManager
 
 
 def main():
-    car = Car(2025, 'toyota')
+    car = Car(2005, "toyota")
 
     car.get_speed()
     for _ in range(5):
@@ -14,5 +15,11 @@ def main():
         car.brake()
 
     print(car.get_speed())
+
+    cm = CarManager("./cars.csv")
+    print(cm.get_car_list())
+    print(cm.get_cars_older_than(10))
+    print(cm.get_cars_of_category("hatchback"))
+
 
 main()
