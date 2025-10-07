6f6848c94ddb90033bbd07de7ab54896409cbb85
atharva.tikhe <atharva.tikhe@medgenome.com>
2025-10-07 12:01:49 +0100

    init

---
+++ assessment/question_manager.py
@@ -0,0 +1,40 @@
+class QuestionManager:
+
+    def __init__(self, questions=None):
+        pass
+
+    def get_questions(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def add_question(self, question):
+        pass
+
+    def remove_question(self, question):
+        pass
+
+    def edit_question(self, old_question, new_question):
+        pass
+
+    def search_by_text(self, text):
+        pass
+
+    def search_by_total_marks(self, total_marks):
+        pass
+
+    def get_questions_by_topic_and_last_attempt(self, topic, last_attempt):
+        pass
+
+    def get_category_count_per_topic(self):
+        pass
+
+    def load_from_file(self, file_name):
+        pass
+
+    def save_to_file(self, file_name):
+        pass
